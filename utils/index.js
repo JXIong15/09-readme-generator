398e3c6f4e16ea6d1fb6317203ea1e92096abb46
Pikachu <jouxiong15@gmail.com>
2021-03-31 22:46:14 -0500

    added questions to be prompted and initialized packages

---
+++ utils/index.js
@@ -0,0 +1,74 @@
+// Include packages needed for this application
+const inquirer = require("inquirer");
+const fs = require("fs");
+const path = require("path");
+const generateMarkdown = require("./generateMarkdown");
+
+// Array of questions for user input
+const questions = [
+    {
+        type: 'input',
+        name: 'username',
+        message: 'What is your GitHub username?'
+    },
+    {
+        type: 'input',
+        name: 'email',
+        message: 'What is your email address?'
+    },
+    {
+        type: 'input',
+        name: 'project',
+        message: 'What is your project title?'
+    },
+    {
+        type: 'input',
+        name: 'description',
+        message: 'Write a short description of your project.'
+    },
+    {
+        type: 'list',
+        name: 'license',
+        message: 'What kind of lisence should your project have?',
+        choices: ['MIT', 'APACHE 2.0', 'GPL 3.0', 'BSD 3', 'None']
+    },
+    {
+        type: 'input',
+        name: 'install',
+        message: 'What command should be run to install dependencies?',
+        default: 'npm i'
+    },
+    {
+        type: 'input',
+        name: 'tests',
+        message: 'What command should be run to run tests?',
+        default: 'npm test'
+    },
+    {
+        type: 'input',
+        name: 'userKnowledge',
+        message: 'What does the user need to know about using the repo?'
+    },
+    {
+        type: 'input',
+        name: 'userContribution',
+        message: 'What does the user need to know about contributing to the repo?',
+    }
+];
+
+// TODO: Create a function to write README file
+function writeToFile(fileName, data) {
+    let testMarkdown = "#HELLO";
+    return fs.writeFileSync(path.join(process.cwd(), fileName), data)
+}
+
+// Initializes app by prompting the questions for the desired ReadMe file.
+function init() {
+    inquirer.prompt(questions).then((responses) => {
+        writeToFile("README.md", generateMarkdown);
+        console.log(responses);
+    })
+}
+
+// Function call to initialize app
+init();
